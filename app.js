eacb8afca7a2265a8e447ad8ced85e503a7a753a
Hans <h.henrik97@gmail.com>
2018-09-12 13:47:10 +0300

    heroku jaoks v1

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+	res.send("homepage");
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
