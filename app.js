bd7938503695595f684eceb120f4f05f98213a82
Hans <h.henrik97@gmail.com>
2018-10-01 15:24:56 +0300

    lisasin page3 ja page4 routid app.js-i

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ app.get("/", function(req, res){
 app.get("/second", function(req, res){
 	res.sendFile(path.join(__dirname+'/views/page2.html'));
 });
-app.get("/hans", function(req, res){
-	res.sendFile(path.join(__dirname+'/views/hansuproov.html'));
+app.get("/third", function(req, res){
+	res.sendFile(path.join(__dirname+'/views/page3.html'));
+});
+app.get("/fourth", function(req, res){
+	res.sendFile(path.join(__dirname+'/views/page4.html'));
 });
 app.get("*", function(req, res){
 	res.send("Seda lehte ei eksisteeri.");
