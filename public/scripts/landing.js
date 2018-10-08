aac673883148cc315e10da0b1c6319fb21b4a84a
janna98 <Janna-Liina.Leemets@tudeng.ut.ee>
2018-10-08 15:22:51 +0300

    Tegin esilehele validatsiooni parandused
    CSS ja HTML

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -11,6 +11,11 @@ $("#nextbutton").click(function(){
 	location.href = "/second";
 });
 
+$(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.form-inline').find('.dropdown-toggle').html(selText);
+});
+/*
 $(function(){
 	$(".dropdown-menu").click(function(){
 	  $(this).parents(".form-inline").find('.btn').html($(this).text());
