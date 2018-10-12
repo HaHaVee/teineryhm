7b81ee4a7452a3402adbdb209f72da54760d6d4a
janna98 <Janna-Liina.Leemets@tudeng.ut.ee>
2018-10-12 15:43:17 +0300

    Valideerisin lehti

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -11,14 +11,7 @@ $("#nextbutton").click(function(){
 	location.href = "/second";
 });
 
-$(".dropdown-menu li a").click(function(){
+$(".dropdown-menu a").click(function(){
   var selText = $(this).text();
   $(this).parents('.form-inline').find('.dropdown-toggle').html(selText);
-});
-/*
-$(function(){
-	$(".dropdown-menu").click(function(){
-	  $(this).parents(".form-inline").find('.btn').html($(this).text());
-	  $(this).parents(".form-inline").find('.btn').val($(this).data('value'));
-	});
 });
