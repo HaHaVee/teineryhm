419c89d30d4aafb834fd56d5e6b5ce41957ccfb7
Hans <h.henrik97@gmail.com>
2018-10-18 23:19:55 +0300

    removed /login/ from auth callbackURL

--- models/auth.js
+++ models/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : '179724612944070',
 		'clientSecret' : '2de6d634b0831d22b1caf563ec33a4b3',
-		'callbackURL' : 'https://blooming-mesa-29689.herokuapp.com/login/auth/facebook/callback'
+		'callbackURL' : 'https://blooming-mesa-29689.herokuapp.com/auth/facebook/callback'
 	}
 }
