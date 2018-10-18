4c136d9d823ca5a3c89ec1e9789da466f3bf2079
Hans <h.henrik97@gmail.com>
2018-10-18 23:28:24 +0300

    changed app.js function inside FBStrat

--- app.js
+++ app.js
@@ -36,28 +36,8 @@ passport.use(new FacebookStrategy({
     callbackURL: configAuth.facebookAuth.callbackURL
   },
   function(accessToken, refreshToken, profile, cb) {
-    process.nextTick(function(){
-    	User.findOne({'facebook.id' : profile.id}, function(err, user){
-    		if (err){
-    			return done(err);
-    		}
-    		if (user){
-    			return done(null, user);
-    		}
-    		else {
-    			var newUser = new User();
-    			newUser.facebook.id = profile.id;
-    			newUser.facebook.token = accessToken;
-    			newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-
-    			newUser.save(function(err){
-    				if (err){
-    					throw err;
-    				}
-    				return done(null, newUser);
-    			});
-    		}
-    	});
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
     });
   }
 ));
