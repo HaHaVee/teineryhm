c65e0ad74c2cf2360225789946329af35b09e4bf
Hans <h.henrik97@gmail.com>
2018-12-02 18:57:38 +0200

    wedriverIO setup

---
+++ node_modules/wdio-selenium-standalone-service/test/integration/wdio.conf.js
@@ -0,0 +1,38 @@
+exports.config = {
+    specs: [
+        './test/integration/*.spec.js'
+    ],
+
+    capabilities: [{
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', '--disable-gpu']
+        }
+    }],
+
+    sync: true,
+    logLevel: 'verbose',
+    coloredLogs: true,
+
+    baseUrl: 'http://webdriver.io',
+
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd'
+    },
+
+    services: [
+        require('../../launcher')
+    ],
+    seleniumLogs: './',
+    seleniumArgs: {
+        version: '2.45.0'
+    },
+    seleniumInstallArgs: {
+        version: '2.45.0'
+    }
+}
